ffb71659420a2a6e8583d311fb790c7ea6cdda2f
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 02:21:18 +0000

    no

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,1 +1,2 @@
-open("about:blank", "", 400,400)
+var win = window.open('about:blank', '_blank', 100, 100); 
+win.document.write("<DOCTYPE html> <html> <head> <title> JAMES </title> </head><body></body> </html>")
