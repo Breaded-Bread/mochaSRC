db2566618032efc8363d2020adb69009af698c39
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 02:51:58 +0000

    changes

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,2 +1,2 @@
 var win = window.open('about:blank', '_blank', 100, 100); 
-win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p> <iframe src'https://.com'> </iframe> </body> </html>")
+win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p> <iframe name='frame' title='k' src='https://breaded-bread.github.io/mochaSRC/funny.png'> </iframe><p href='https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/' target='frame'>Open</p> </body> </html>")
