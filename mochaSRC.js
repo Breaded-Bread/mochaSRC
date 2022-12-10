fd892fd299c940a60c1f6e06f7b75cf0b1c70089
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 04:03:35 +0000

    passcode fix

--- mochaSRC.js
+++ mochaSRC.js
@@ -2,12 +2,15 @@ function openNebula() {
   open("https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/", "", 100, 100);
 }
 
-var passwordinput = document.getElementById("pass").value;
 
-if (passwordinput == "Mochascript") {
-  openNebula();
-}
-// Commit 
+setInterval(function() {
+  var passwordinput = document.getElementById("pass").value;
+  if (passwordinput == "Mochascript") {
+    openNebula();
+    }
+  }
+, 1)
+
 
 
 
