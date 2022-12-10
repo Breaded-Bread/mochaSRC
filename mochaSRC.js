9c15befeb17cc30f4034d18f002b4be98f00f4c0
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 02:31:31 +0000

    aight it shold wok now

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,2 +1,2 @@
 var win = window.open('about:blank', '_blank', 100, 100); 
-win.document.write("<DOCTYPE html> <html> <head> <title> JAMES </title> </head><body></body> </html>")
+win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p></body> </html>")
