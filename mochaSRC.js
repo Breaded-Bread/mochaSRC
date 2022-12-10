eeef31c614f64ee8fa714d78b0c0da9ac37c7016
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 03:46:39 +0000

    op af ngl

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,2 +1,3 @@
-var win = window.open('about:blank', '_blank'); 
-win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><iframe name='frame' title='k' src='https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/' height='100%' width='100%'> </iframe></body> </html>")
+function openNebula() {
+  open("https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/", "", 100, 100);
+}
