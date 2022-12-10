43a62736090e7ad06fe0bc4274123a5a33d5859f
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 02:38:12 +0000

    idk

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,2 +1,2 @@
 var win = window.open('about:blank', '_blank', 100, 100); 
-win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p>  </body> </html>")
+win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p> <iframe src'https://.com'> </iframe> </body> </html>")
