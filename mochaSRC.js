85e91c27263ee388ea8a47a2575d2cecc59f9ab4
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-10 23:35:01 +0000

    Made extra forum elements work (with CSS)

--- mochaSRC.js
+++ mochaSRC.js
@@ -9,14 +9,26 @@ function openNebula() {
 
 
 const newLocal = "Mochascript";
+var cb1 = 0
+var cb2 = 0
+var cb3 = 0
 setInterval(function() {
   var passwordinput = document.getElementById("popo").value;
+  cb1 = document.getElementById("checkb1");
+  cb2 = document.getElementById("checkb2");
+  cb3 = document.getElementById("checkb3");
+  console.log(cb1+cb2+cb3)
+  if (cb1 == true && cb2 == true && cb3 == true){
   if (passwordinput == newLocal) {
     openNebula();
     }
+  }
   }
 , 100)
 
 
 //javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/FogNetwork/Ingot/ingot.min.js'; document.body.appendChild(a);}())
+
+// proxy down
+
 
