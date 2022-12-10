9b0a4f8103dcc26c9527147d6b0fd615c908abe1
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 03:56:08 +0000

    passcode

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,3 +1,30 @@
 function openNebula() {
   open("https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/", "", 100, 100);
 }
+
+var passwordinput = document.getElementById("pass").value;
+
+if (passwordinput == "Mochascript") {
+  openNebula();
+}
+// Commit 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
