7f6184be5c33041200f8d5ecd12897a6d8808580
Preston Davis <106452061+Breaded-Bread@users.noreply.github.com>
2022-12-10 03:30:25 +0000

    much better i think

--- mochaSRC.js
+++ mochaSRC.js
@@ -1,2 +1,2 @@
-var win = window.open('about:blank', '_blank', 100, 100); 
-win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><p>we love cookies!!!</p> <iframe name='frame' title='k' src='https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/'' style='height:500px;width:1000px;'> </iframe></body> </html>")
+var win = window.open('about:blank', '_blank'); 
+win.document.write("<DOCTYPE html> <html> <head> <title> cookies </title> </head><body><iframe name='frame' title='k' src='https://3000-breadedbrea-breadmathhe-masiztl7npg.ws-us78.gitpod.io/' height='100%' width='100%'> </iframe></body> </html>")
