7c0c6fc93da5b593840a3114f6c268c79465babc
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-11 00:17:54 +0000

    Made Javascript interpret Hypertext object states and check for correctness

--- mochaSRC.js
+++ mochaSRC.js
@@ -4,18 +4,21 @@ function openNebula() {
 
 
 const newLocal = "Mochascript";
-
 var passwordinput = document.getElementById("popo").value;
-
-const cb = document.querySelector('#checkb1');
-
+const cb1 = document.querySelector('#checkb1');
+const cb2 = document.querySelector('#checkb3');
+const cb3 = document.querySelector('#checkb4');
 const btn = document.querySelector('#button');
 
 btn.onclick = () => {
-console.log(cb);
-
-
-    if(cb == true){
-      
+    if(cb1.checked == true){
+      if(cb2.checked == true){
+        if(cb3.checked == true){
+        passwordinput = document.getElementById("popo").value;
+          if(passwordinput==newLocal){
+            openNebula();
+          }
+        }
+        }
     }
 }
